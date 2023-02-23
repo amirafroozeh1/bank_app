99c2608bb2a6419cf9f169ae0f26d5af1760be29
amir <amirafroozeh1@users.noreply.github.com>
2023-02-23 18:02:57 +0100

    Fix config

--- account-service/src/main/java/com.account.app/RabbitMQConfig.java
+++ account-service/src/main/java/com.account.app/RabbitMQConfig.java
@@ -5,11 +5,39 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    @Bean
+    public Queue jsonQueue(){
+        return new Queue(jsonQueue);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding jsonBinding(){
+        return BindingBuilder
+                .bind(jsonQueue())
+                .to(exchange())
+                .with(routingJsonKey);
+    }
 
     @Bean
     public MessageConverter converter(){
